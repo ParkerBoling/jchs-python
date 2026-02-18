dcaf224dfd29f8d7f7c2be0f4d8e015814769a30
Parker Boling <parkerdboling@gmail.com>
2026-02-18 13:42:54 -0800

    caesarCypher Code

---
+++ Lab 6/caesarCypher - 2.py
@@ -0,0 +1,44 @@
+# INPUT #
+'''
+sentence
+sh
+'''
+
+def caesarCipher(text, shift, dir):
+  dir = dir.lower()
+  if dir == "left":
+    shift *= -1
+  
+  result = ""
+
+  for char in text:
+      if char.isalpha():
+          if char.isupper():
+              start = ord('A')
+          else:
+              start = ord('a')
+
+          encryptedChar = chr((ord(char) - start + shift) % 26 + start)
+          result += encryptedChar
+      else:
+          result += char
+
+  return result
+
+def main():
+  choice = "yes"
+
+  while choice.lower() == "yes":
+    text = input("Enter a word, phrase, or sentence: ")
+    shift = int(input("Enter a shift value: "))
+    dir = input("Enter the shift direction: ")
+    
+
+    encrypted = caesarCipher(text, shift, dir)
+    print("Encrypted sentence:", encrypted)
+
+    choice = input("Would you like to encode another message? (yes/no): ")
+    print()
+
+if __name__ == "__main__":
+  main()
