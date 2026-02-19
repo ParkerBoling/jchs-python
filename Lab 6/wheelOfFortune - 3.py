12f7eb2670624e6bf3a2d6d148ff63c06e8dbaf2
Parker Boling <parkerdboling@gmail.com>
2026-02-19 14:20:42 -0800

    add example

--- Lab 6/wheelOfFortune - 3.py
+++ Lab 6/wheelOfFortune - 3.py
@@ -28,11 +28,21 @@ You have guessed: ['r']
 ____r ___ _r____
 '''
 
+import random
+
 def getPuzzle():
 
 def displayPuzzle(name):
-    guesses = [" "]
-    
+    """This function returns a tuple (Category, Phrase) that can be used to play a simple game of like Wheel of Fortune."""
+    puzzles = [("Famous People", "Abraham Lincoln"), ("Famous People", "Oprah Winfrey"), ("Famous People", "Will Smith"), ("Famous People", "Marilyn Monroe"), ("Action Movie", 'Mad Max: Fury Road'),  ("Action Movie", 'Die Hard'),  ("Action Movie", 'John Wick'),  ("Action Movie", 'Enter the Dragon')]
+    index = random.randint(0, len(puzzles)-1)
+    return puzzles[index]
+
+
+def showGuesses():
+
+
+def showBoard(guesses, name):
 
 
 def main():
