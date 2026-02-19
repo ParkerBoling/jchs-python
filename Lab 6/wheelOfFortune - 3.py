fc6bed578f369c066301bfb284ceb3aa1df00b2c
Parker Boling <parkerdboling@gmail.com>
2026-02-19 14:34:59 -0800

    az;lsjdfg;kjasg

--- Lab 6/wheelOfFortune - 3.py
+++ Lab 6/wheelOfFortune - 3.py
@@ -31,19 +31,30 @@ ____r ___ _r____
 import random
 
 def getPuzzle():
-
-def displayPuzzle(name):
     """This function returns a tuple (Category, Phrase) that can be used to play a simple game of like Wheel of Fortune."""
     puzzles = [("Famous People", "Abraham Lincoln"), ("Famous People", "Oprah Winfrey"), ("Famous People", "Will Smith"), ("Famous People", "Marilyn Monroe"), ("Action Movie", 'Mad Max: Fury Road'),  ("Action Movie", 'Die Hard'),  ("Action Movie", 'John Wick'),  ("Action Movie", 'Enter the Dragon')]
     index = random.randint(0, len(puzzles)-1)
     return puzzles[index]
 
+def displayPuzzle(name):
+    """This function returns a tuple (Category, Phrase) that can be used to play a simple game of like Wheel of Fortune."""
+    puzzles = [("Famous People", "Abraham Lincoln"), ("Famous People", "Oprah Winfrey"), ("Famous People", "Will Smith"), ("Famous People", "Marilyn Monroe"), ("Action Movie", 'Mad Max: Fury Road'),  ("Action Movie", 'Die Hard'),  ("Action Movie", 'John Wick'),  ("Action Movie", 'Enter the Dragon')]
+    return random.choice(puzzles)
+
 
 def showGuesses():
+    
 
+def showBoard(guesses, phrase):
+    board = ""
 
-def showBoard(guesses, name):
+    for letter in phrase:
+        if letter.lower() in guesses or not letter.isalpha():
+            board += letter
+        else:
+            board += "_"
 
+    print("\n" + board)
 
 def main():
 
