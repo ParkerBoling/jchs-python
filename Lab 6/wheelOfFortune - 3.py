26ba1f96d54dedbe7a8d5c390655e0d176e01b6a
Parker Boling <parkerdboling@gmail.com>
2026-02-19 14:17:58 -0800

    start code

--- Lab 6/wheelOfFortune - 3.py
+++ Lab 6/wheelOfFortune - 3.py
@@ -4,14 +4,35 @@
 '''
 # PROCESS #
 '''
+get random number
+use number to select categoruy
+get guess
+check if in phrase
+if in phrase place letter in a set of underscores
 
 '''
 # OUTPUT #
 '''
+Welcome to Wheel of Fortune!
+You have to guess the phrase before you make 7 wrong guesses
+Good luck
 
+Your pusszle is from the category: category (ex Action Movie)
+
+_________ ______ ________
+
+what letter would you like?
+R
+You have guessed: ['r']
+
+____r ___ _r____
 '''
 
-def wheelOfFortune():
+def getPuzzle():
+
+def displayPuzzle(name):
+    guesses = [" "]
+    
 
 
 def main():
